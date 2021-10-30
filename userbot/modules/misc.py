da05d6e088ed98c4e1e84b67cb52e3e41ce86995
evolos <86545693+evolos@users.noreply.github.com>
2021-10-30 20:04:13 +0700

    Update misc.py

--- userbot/modules/misc.py
+++ userbot/modules/misc.py
@@ -99,7 +99,7 @@ async def repeat(rep):
 async def repo_is_here(wannasee):
     """ For .repo command, just returns the repo URL. """
     await wannasee.edit(
-        "[Repo](https://github.com/adekmaulana/ProjectBish) GitHub's page."
+        "[Klik disini](https://github.com/evolos/Evobot) GitHub's page."
     )
 
 
