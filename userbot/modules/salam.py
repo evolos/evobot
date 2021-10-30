283e06c91ca57143bd2533d8b188f86740c972ce
evolos <86545693+evolos@users.noreply.github.com>
2021-10-30 19:22:37 +0700

    Add files via upload

---
+++ userbot/modules/salam.py
@@ -0,0 +1,44 @@
+from time import sleep
+from platform import uname
+from userbot import CMD_HELP
+from userbot.events import register
+
+@register(outgoing=True, pattern='^P(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("`Assalamualaikum.....`")
+# Owner @Si_Dian
+
+
+@register(outgoing=True, pattern='^p(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("`Assalamualaikum.....`")
+# Owner @Si_Dian
+
+
+@register(outgoing=True, pattern='^L(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("`Waallaikumsalam......`")
+# Owner @Si_Dian
+
+
+@register(outgoing=True, pattern='^l(?: |$)(.*)')
+async def typewriter(typew):
+    typew.pattern_match.group(1)
+    sleep(1)
+    await typew.edit("`Waallaikumsalam.....`")
+# Owner @Si_Dian
+
+
+CMD_HELP.update({
+    "salam":
+    "`P`\
+\nUsage: Untuk Memberi salam.\
+\n\n`L`\
+\nUsage: Untuk Menjawab Salam."
+})
